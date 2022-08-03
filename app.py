057c84a3538f980e8b2f0da43f413770788ef04b
Connor Chow <legoman888@icloud.com>
2022-08-02 23:06:27 -0700

    Created basic app file

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 import logging
 import os
+
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+
+app = App(token=SLACK_BOT_TOKEN)
+
+@app.event("app_mention")
+def mention_handler(body, context, payload, options, say, event):
+    say("Hello World!")
+
+@app.event("message")
+def message_handler(body, context, payload, options, say, event):
+    pass
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler.start()
