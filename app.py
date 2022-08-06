62ce86fbf15afa8cf04187d99085101a9a9a2e5e
Connor Chow <legoman888@icloud.com>
2022-08-05 23:18:50 -0700

    added example parts + updating app mentions & dms

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import logging
 import os
 
@@ -13,18 +14,34 @@ SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=SLACK_BOT_TOKEN)
 
+### command example(not used by my bot) ###
+# # This will match any message that contains 👋
+# @app.message(":wave:")
+# def say_hello(message, say):
+#     user = message['user']
+#     say(f"Hi there, <@{user}>!")
+#
+# @app.command("/hello-socket-mode")
+# def hello_command(ack, body):
+#     user_id = body["user_id"]
+#     ack(f"Hi, <@{user_id}>!")
+    
+### example function head - def message_handler(body, context, payload, options, say, event): ###
+
 @app.event("app_mention")
 def mention_handler(body, context, payload, options, say, event):
-    say("Hello World!")
-
+    text = body['text']
+    say(text)
+    calendarMaker(say)
 @app.event("message")
-def message_handler(body, context, payload, options, say, event):
+def message_handler(message, body, context, payload, options, say, event):
+    text = message['text']
+    say(text)
     calendarMaker(say)
     # pass
 
 def calendarMaker(say):
     say("Hello World!!")
-    pass
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
